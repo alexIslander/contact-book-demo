c251f9f9051acefb1c4e954d82a5e24dc5f75f17
AlexIslander <sandor@csetesandor.hu>
2021-11-09 13:33:21 +0100

    base layout
    - creation of components(list, view, form)
    - html structure for component arrangement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactViewComponent } from './contact-view/contact-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactFormComponent,
+    ContactListComponent,
+    ContactViewComponent
   ],
   imports: [
     BrowserModule,
